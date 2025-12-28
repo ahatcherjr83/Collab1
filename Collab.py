17aa95d285215f846478c94bbd98710bee2e48b4
ahatcherjr83 <ahatcherjr83@icloud.com>
2025-12-28 04:15:35 -0800

    Create Collab.py

---
+++ Collab.py
@@ -0,0 +1,25 @@
+KEYWORDS = [
+    # existing art-clothing
+    "uniqlo.*kaws", "comme des garcons.*oni", "lv.*murakami",
+    "dior.*arsham", "supreme.*basquiat", "mcqueen.*hirst",
+    "takashi murakami", "daniel arsham", "jean-michel basquiat",
+    "keith haring.*uniqlo", "frida kahlo.*toms", "lawrence weiner.*lv",
+
+    # shoe-brand collabs (art + designer)
+    "nike.*kaws", "nike.*pharrell", "nike.*jacquemus", "nike.*nigo",
+    "nike.*travis scott", "nike.*off-white", "nike.*sacai",
+    "adidas.*pharrell", "adidas.*bad bunny", "adidas.*willy chavarria",
+    "adidas.*hagel", "puma.*san san gear", "new balance.*ganni",
+    "new balance.*miu miu", "new balance.*comme des garcons",
+    "salomon.*margiela", "salomon.*mm6", "hoka.*marni", "on.*loewe",
+    "vans.*valentino", "vans.*calleja", "converse.*naruto",
+    "asics.*kith", "merrell.*gramicci", "moon boot.*guest in residence",
+
+    # GU capsules (keep them in one pipe-separated monster for brevity)
+    "gu.*harry potter", "gu.*chiikawa", "gu.*rolling stones",
+    "gu.*imase", "gu.*sanrio", "gu.*hello kitty", "gu.*pompompurin",
+    "gu.*ac/dc", "gu.*death stranding", "gu.*museum of ice cream",
+    "gu.*rokh", "gu.*soph", "gu.*kuromi", "gu.*esther bunny",
+    "gu.*spongebob", "gu.*batman", "gu.*spotify", "gu.*movie",
+    "gu.*omiyage", "gu.*ug.*undercover"
+]
